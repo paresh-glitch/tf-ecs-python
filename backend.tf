f294bf4b457de597b5d34919553d673e0ace97f7
aez <aez@email.com>
2026-06-19 15:50:05 +0530

    modify: terraform.yml backend.tf add destroy.yml

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "paresh-tf-state-bucket" # The exact bucket from step 1
-    key            = "prod/ecs/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-locks" # The exact table from step 1
     encrypt        = true
